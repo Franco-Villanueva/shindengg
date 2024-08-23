63b8f8c330c50482aa3efa8b96ef0c0c42b6ab0e
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-08-23 15:16:00 -0300

    reseteo y configuracion

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../index.css'; 
+
+const Main = () => {
+    return ( 
+        <>
+        <h1 className="text-red-500 bg-blue-200">Este es un título en rojo</h1>
+        
+        </>
+     );
+}
+ 
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Verifica que este patrón coincida con tus archivos de código.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
