99698c8692c83c1b7c75b248e32d44167375f65a
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-10-17 18:02:36 -0300

    estilos en los videos

--- src/Components/LastVideos/LastVideos.jsx
+++ src/Components/LastVideos/LastVideos.jsx
@@ -17,7 +17,7 @@ const LastVideos = () => {
           title: item.snippet.title,
           thumbnail: item.snippet.thumbnails.medium.url,
         }));
-        console.log(data)
+        console.log(data);
         setVideos(videoItems);
       })
       .catch((error) => {
@@ -28,22 +28,45 @@ const LastVideos = () => {
   }, []);
 
   return (
-    <div className="videos-container">
+    <div className="flex items-center justify-center min-h-96 bg-[#1b1a1b] p-2 pt-16 xl:mx-52 lg:mx-24 md:mx-14 sm:mx-6">
       {Videos.length === 0 ? (
         <p>Loading videos...</p>
       ) : (
-        Videos.map((video) => (
-          <div key={video.videoId} className="video-item">
+        <div className="grid grid-cols-3 gap-4 bg-[#f9b6f9] rounded-3xl p-10">
+          <div className="h-full col-span-2">
             <a
-              href={`https://www.youtube.com/watch?v=${video.videoId}`}
+              href={`https://www.youtube.com/watch?v=${Videos[0].videoId}`}
               target="_blank"
               rel="noopener noreferrer"
+              className="block"
             >
-              <img src={video.thumbnail} alt={video.title} />
-              <h3>{video.title}</h3>
+              <img
+                src={Videos[0].thumbnail}
+                alt={Videos[0].title}
+                className="object-cover w-full h-auto rounded-3xl"
+              />
+              <h3 className="mt-2 text-lg text-white">{Videos[0].title}</h3>
             </a>
           </div>
-        ))
+          <div className="flex flex-col space-y-4">
+            {Videos.slice(1).map((video) => (
+              <a
+                key={video.videoId}
+                href={`https://www.youtube.com/watch?v=${video.videoId}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block"
+              >
+                <img
+                  src={video.thumbnail}
+                  alt={video.title}
+                  className="object-cover w-full h-auto rounded-3xl"
+                />
+                <h3 className="mt-2 text-sm text-white">{video.title}</h3>
+              </a>
+            ))}
+          </div>
+        </div>
       )}
     </div>
   );
